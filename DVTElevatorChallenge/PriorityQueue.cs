4c9a42e8c6337691b578bc4b0b595310466f1d11
WilliamMorake <veger@tuks.co.za>
2023-08-07 03:39:07 +0200

    Adding updated code including unit test

---
+++ DVTElevatorChallenge/PriorityQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVTElevatorChallenge
+{
+    // Implementation of a simple Priority Queue using a Dictionary
+    public class PriorityQueue<TKey, TValue> where TKey : IComparable<TKey>
+    {
+        private SortedDictionary<TKey, Queue<TValue>> queue = new SortedDictionary<TKey, Queue<TValue>>();
+
+        public void Enqueue(TValue value, TKey priority)
+        {
+            if (!queue.ContainsKey(priority))
+            {
+                queue.Add(priority, new Queue<TValue>());
+            }
+            queue[priority].Enqueue(value);
+        }
+
+        public (TValue, TKey) Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Priority queue is empty.");
+            }
+            var firstKey = queue.Keys.First();
+            var value = queue[firstKey].Dequeue();
+            if (queue[firstKey].Count == 0)
+            {
+                queue.Remove(firstKey);
+            }
+            return (value, firstKey);
+        }
+
+        public bool IsEmpty => queue.Count == 0;
+    }
+
+}
---
+++ NUnitTestProject1/ElevatorControllerTests.cs
@@ -0,0 +1,61 @@
+using DVTElevatorChallenge;
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ElevatorControllerTests
+    {
+        private ElevatorController elevatorController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            int numFloors = 10;
+            int numElevators = 1;
+            int maxCapacity = 8;
+
+            elevatorController = new ElevatorController(numFloors, numElevators, maxCapacity);
+        }
+
+        [Test]
+        public void CallElevator_ElevatorIsAvailable_ElevatorMovesToRequestedFloor()
+        {
+            // Arrange
+            int floorNumber = 5;
+            int lastfloorNumber = 7;
+            elevatorController.SetWaitingPassengers(floorNumber, 4, lastfloorNumber); // 2 passengers on floor 5 going to floors 4 and 7
+
+            // Act
+            elevatorController.CallElevator(floorNumber);
+
+            // Assert
+            var elevators = elevatorController.GetElevators();
+            var elevator = elevators[0]; // Assuming there's only one elevator in the list
+            Assert.AreEqual(lastfloorNumber, elevator.CurrentFloor);
+        }
+
+        [Test]
+        public void SetWaitingPassengers_AddWaitingPassengers_ElevatorHasDestinationFloors()
+        {
+            // Arrange
+            int floorNumber = 3;
+            int[] destinationFloors = new int[] { 1, 6, 9 };
+
+            // Act
+            elevatorController.SetWaitingPassengers(floorNumber, destinationFloors);
+            elevatorController.CallElevator(3);
+
+            // Assert
+            var elevators = elevatorController.GetElevators();
+            var elevator = elevators[0]; // Assuming there's only one elevator in the list
+            var destinationFloorsInElevator = elevator.GetDestinationFloors();
+            Assert.AreEqual(destinationFloors.Length, destinationFloorsInElevator.Count);
+            Assert.AreEqual(destinationFloors[0], destinationFloorsInElevator[0]);
+            Assert.AreEqual(destinationFloors[1], destinationFloorsInElevator[1]);
+            Assert.AreEqual(destinationFloors[2], destinationFloorsInElevator[2]);
+        }
+    }
+
+}
